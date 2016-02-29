c3e3378042ab4568857e2ed4b90038178c4007d0
Kamil Hakim <kamil.hakim.swe@gmail.com>
2016-02-29 16:53:31 +0100

    added a plugin to check file count

---
+++ manifests/plugins/checkfilecount.pp
@@ -0,0 +1,38 @@
+# == Class: icinga::plugins::chechfilecount
+#
+# This class provides a checkfilecount plugin.
+#
+class icinga::plugins::checkfilecount (
+  $check_warning         = '1',
+  $check_critical        = '0',
+  $target_folder         = undef,
+  $max_check_attempts    = $::icinga::max_check_attempts,
+  $contact_groups        = $::environment,
+  $notification_period   = $::icinga::notification_period,
+  $notifications_enabled = $::icinga::notifications_enabled,
+) inherits icinga {
+
+  if $icinga::client {
+    file{"${::icinga::includedir_client}/check_file_count.cfg":
+      ensure  => 'file',
+      mode    => '0644',
+      owner   => $::icinga::client_user,
+      group   => $::icinga::client_group,
+      content => "command[check_file_count]=sudo ${::icinga::plugindir}/check_file-count.sh -w ${check_warning} -c ${check_critical} -f ${target_folder}\n",
+      notify  => Service[$::icinga::service_client],
+    }
+
+    @@nagios_service { "check_nginx_routing_file_count_${::fqdn}":
+      check_command         => 'check_nrpe_command!check_file_count',
+      service_description   => 'Routing files',
+      host_name             => $::fqdn,
+      contact_groups        => $contact_groups,
+      max_check_attempts    => $max_check_attempts,
+      notification_period   => $notification_period,
+      notifications_enabled => $notifications_enabled,
+      target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
+      action_url            => '/pnp4nagios/graph?host=$HOSTNAME$&srv=$SERVICEDESC$',
+    }
+  }
+
+}
