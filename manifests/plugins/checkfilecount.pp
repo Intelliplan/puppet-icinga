05aeec0d3546745a894a6282a132797fa4c426dd
Kamil Hakim <kamil.hakim.swe@gmail.com>
2016-03-01 11:38:29 +0100

    some simplifications

--- manifests/plugins/checkfilecount.pp
+++ manifests/plugins/checkfilecount.pp
@@ -13,33 +13,28 @@ class icinga::plugins::checkfilecount (
   $notifications_enabled = $::icinga::notifications_enabled,
 ) inherits icinga {
 
-  if $icinga::client {
-    if $::osfamily != 'Debian' {
-      package{$pkgname:
-        ensure => 'installed',
-      }
-    }
-
-    file{"${::icinga::includedir_client}/check_file_count.cfg":
-      ensure  => 'file',
-      mode    => '0644',
-      owner   => $::icinga::client_user,
-      group   => $::icinga::client_group,
-      content => "command[check_file_count]=sudo ${::icinga::plugindir}/check_file-count.sh -w ${check_warning} -c ${check_critical} -f ${target_folder}\n",
-      notify  => Service[$::icinga::service_client],
-    }
-
-    @@nagios_service { "check_nginx_routing_file_count_${::fqdn}":
-      check_command         => 'check_nrpe_command!check_file_count',
-      service_description   => 'Routing files',
-      host_name             => $::fqdn,
-      contact_groups        => $contact_groups,
-      max_check_attempts    => $max_check_attempts,
-      notification_period   => $notification_period,
-      notifications_enabled => $notifications_enabled,
-      target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
-      action_url            => '/pnp4nagios/graph?host=$HOSTNAME$&srv=$SERVICEDESC$',
-    }
+  package{$pkgname:
+    ensure => 'installed',
   }
 
+  file{"${::icinga::includedir_client}/check_file_count.cfg":
+    ensure  => 'file',
+    mode    => '0644',
+    owner   => $::icinga::client_user,
+    group   => $::icinga::client_group,
+    content => "command[check_file_count]=sudo ${::icinga::plugindir}/check_file-count.sh -w ${check_warning} -c ${check_critical} -f ${target_folder}\n",
+    notify  => Service[$::icinga::service_client],
+  }
+
+  @@nagios_service { "check_nginx_routing_file_count_${::fqdn}":
+    check_command         => 'check_nrpe_command!check_file_count',
+    service_description   => 'Routing files',
+    host_name             => $::fqdn,
+    contact_groups        => $contact_groups,
+    max_check_attempts    => $max_check_attempts,
+    notification_period   => $notification_period,
+    notifications_enabled => $notifications_enabled,
+    target                => "${::icinga::targetdir}/services/${::fqdn}.cfg",
+    action_url            => '/pnp4nagios/graph?host=$HOSTNAME$&srv=$SERVICEDESC$',
+  }
 }
